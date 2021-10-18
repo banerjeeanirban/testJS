a8a1d8d7a51b28a324b14c78e9e70d5e3658e728
banerjeeanirban <banerjee.anirban@gmail.com>
2021-10-18 12:13:46 -0500

    Update a.js

--- a.js
+++ a.js
@@ -4,7 +4,7 @@ Http.open("GET", url);
 Http.send();
 Http.onreadystatechange = (e) => {
   console.log(Http.responseText)
-data = {some:thing,another:thing}
+data = {some:thing,another:thing};
 http({method: 'GET', url: '[the-target-url]', headers: {'Authorization': '[your-api-key]'}
 });
 $.ajax({
@@ -37,6 +37,7 @@ $.ajax({
 $.ajax({
    type : "POST",
    url : "[the-target-url]",
+  data = {some2:thing2,another2:thing2},
    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', '[your-api-key]');},
    success : function(result) {
        //set your variable to the result
