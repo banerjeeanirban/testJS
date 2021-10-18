f95c78de5b5f173b5d4b05b5bbdabced44dc002d
banerjeeanirban <banerjee.anirban@gmail.com>
2021-10-18 12:11:10 -0500

    Create a.js
    first push

---
+++ a.js
@@ -0,0 +1,48 @@
+const Http = new XMLHttpRequest();
+const url='https://jsonplaceholder.typicode.com/posts';
+Http.open("GET", url);
+Http.send();
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+data = {some:thing,another:thing}
+http({method: 'GET', url: '[the-target-url]', headers: {'Authorization': '[your-api-key]'}
+});
+$.ajax({
+   type : "GET",
+   url : "[the-target-url]",
+   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', '[your-api-key]');},
+   success : function(result) {
+       //set your variable to the result
+   },
+   error : function(result) {
+     //handle the error
+   }
+ });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//gap gap gap
+
+
+$.ajax({
+   type : "POST",
+   url : "[the-target-url]",
+   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', '[your-api-key]');},
+   success : function(result) {
+       //set your variable to the result
+   },
+   error : function(result) {
+     //handle the error
+   }
+ });
+}
