2e29f5570a25eb4cca0d93748dca3a8d2fdc1d1e
banerjeeanirban <banerjee.anirban@gmail.com>
2021-10-28 15:41:03 -0500

    Update JS-axios-example.js

--- JS-axios-example.js
+++ JS-axios-example.js
@@ -2,6 +2,9 @@ axios.get("https://world.openfoodfacts.org/category/pastas/1.json")
   .then(response => {
     // access parsed JSON response data using response.data field
     data = response.data
+    headers: {
+   Authorization: 'Bearer ' + token //the token is a variable which holds the token
+ }
     console.log(data.count)
     console.log(data.products)
   })
@@ -23,6 +26,11 @@ async function postData () {
 
   try {
     const response = await axios.post("/food", food)
+    {
+    headers: {
+      Authorization: 'Bearer ' + varToken
+    }
+    }
     console.log("Request successful!")
   } catch (error) {
     if (error.response) {
